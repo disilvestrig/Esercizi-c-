04a42f94204db35bc0e3ee3134f9d1ee0ea3af7d
Giulio Di Silvestri <42960623+disilvestrig@users.noreply.github.com>
2019-11-03 12:10:17 +0100

    Create esercizio2.cpp
    Inserendo 10 numeri interi, calcola quanti numeri pari sono stati inseriti

---
+++ esercizio2.cpp
@@ -0,0 +1,21 @@
+//Inserendo 10 numeri interi, calcola quanti numeri pari sono stati inseriti
+#include <iostream>
+using namespace std;
+
+int main() {
+  int numeri[10];
+  int npari;
+  for (int i = 0;i <10;i++){
+    cout << "\nInserisci il numero\n";
+    cin >> numeri[i];
+  }
+  cout << "\nI numeri pari che hai inserito sono\n";
+  for (int i = 0;i <10;i++){
+    if (numeri[i]% 2 == 0){
+      cout << numeri[i] << "\n";
+      npari += 1;
+    }
+    
+  }
+  cout << "\nHai inserito in totale "<< npari <<" numeri pari"; 
+}
