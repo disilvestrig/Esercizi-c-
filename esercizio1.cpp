4fd92636c687253acc15285d286d0063b99e19f1
Giulio Di Silvestri <42960623+disilvestrig@users.noreply.github.com>
2019-11-03 11:56:50 +0100

    Create esercizio1.cpp

---
+++ esercizio1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int array[10];
+  int somma = 0;
+  for (int i = 0;i < 10; i++){
+    cout << "\nInserisci numero da sommare\n";
+    cin >> array[i];
+    if(array[i]== 0){
+      break;
+    }}
+  cout << "\nlettura dei dati :\n" ;
+  for (int i = 0;i<10;i++){
+    if (array[i]==0){
+      break;
+    }
+    cout << "\n" << array[i]<< "\n";
+    somma += array[i];
+  }
+  cout << "Somma : " << somma ;
+}
